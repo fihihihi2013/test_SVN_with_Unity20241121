2cc0cba7520403f4defc2b05b70211e7951baa15
fihihihi2013 <125849576+fihihihi2013@users.noreply.github.com>
2024-11-21 17:48:09 +0900

    헬로우월드 바꿨음

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("HelloSVN");
     }
 
     // Update is called once per frame
